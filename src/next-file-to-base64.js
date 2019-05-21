56275ee3caf9f12fcbd3f994a9c6d53e07c2bb4d
afeiship <1290657123@qq.com>
2019-05-21 15:16:35 +0800

    chore: new script

--- src/next-file-to-base64.js
+++ src/next-file-to-base64.js
@@ -1,26 +1,24 @@
-(function () {
-
-  var global = global || this || self || window;
+(function() {
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var NxQueue = nx.Queue || require('next-queue');
   var DONE = 'done';
-  var ARRAY = [];
 
-  nx.fileToBase64 = function (inFile) {
-    const reader = new FileReader();
+  nx.fileToBase64 = function(inFile) {
+    var reader = new FileReader();
     reader.readAsDataURL(inFile);
-    return new Promise(function (resolve) {
-      reader.onload = function (inEvent) {
+    return new Promise(function(resolve) {
+      reader.onload = function(inEvent) {
         resolve(inEvent.target.result);
       };
     });
   };
 
-
-  nx.filesToBase64 = function (inFiles) {
-    var list = nx.map(inFiles, function (_, file) {
-      return function (next) {
-        nx.fileToBase64(file).then(function (inBase64) {
+  nx.filesToBase64 = function(inFiles) {
+    var result = [];
+    var list = nx.map(inFiles, function(_, file) {
+      return function(next) {
+        nx.fileToBase64(file).then(function(inBase64) {
           next(inBase64);
         });
       };
@@ -28,8 +26,8 @@
 
     if (list.length > 0) {
       var queue = new NxQueue(list);
-      return new Promise(function (resolve) {
-        queue.start().then(function (response) {
+      return new Promise(function(resolve) {
+        queue.start().then(function(response) {
           var status = response.status;
           var data = response.data;
           if (status === DONE) {
@@ -39,12 +37,11 @@
       });
     }
     return {
-      then: function(resolve){
-        resolve(ARRAY);
+      then: function(resolve) {
+        resolve(result);
       }
     };
   };
-
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = {
@@ -52,5 +49,4 @@
       filesToBase64: nx.filesToBase64
     };
   }
-
-}());
+})();
